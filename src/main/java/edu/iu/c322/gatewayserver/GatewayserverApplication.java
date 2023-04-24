60208a5f0d08348b64c104a9ea404f22230a3d4a
Aidan Cooper <aidcoope@iu.edu>
2023-04-24 17:39:17 -0400

    Initial Commit

---
+++ src/main/java/edu/iu/c322/gatewayserver/GatewayserverApplication.java
@@ -0,0 +1,13 @@
+package edu.iu.c322.gatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/iu/c322/gatewayserver/GatewayserverApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.iu.c322.gatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
